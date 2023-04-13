ec85463e29064ecfe3d1d7b41d68039f9297db0f
squrem <108416846+squrem@users.noreply.github.com>
2023-04-13 14:39:24 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ from scipy.stats import ttest_ind
 chat_id = 434559054 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-  if ttest_ind(x, y, equal_var=False, alternative=alternative).pvalue < 0.03:
+  if ttest_ind(x, y, equal_var=False, alternative="less").pvalue < 0.03:
     return True
   else:
     return False  
